cdbcb9899ab22b245f735a37cbf675070e53f778
Chetanya <chetanyashrimalie5@gmail.com>
2018-03-23 20:03:04 +0530

    added models
    Signed-off-by: Chetanya <chetanyashrimalie5@gmail.com>

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=250, default="Hello")
+    date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.username + "  " + str(self.date)
+
+
